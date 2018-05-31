8f3449b8d3fb246d9efd856df60c03f5f423b26a
Eugene <eshereverov@gmail.com>
2018-05-31 19:06:59 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict"
+
+function addDiv(node, className, id, msg) {
+  let el = document.createElement(node);
+  el.className = className;
+  el.id = id;
+  el.textContent = msg;
+  document.body.appendChild(el);
+}
+
+addDiv("div", "app", "app", "Hello!!!");
